e70d28921fdf26365f091923f6b695c35515e677
stevemolloy <sdmolloy@gmail.com>
2017-01-08 00:01:07 +0100

    First upload

---
+++ IceCubeIocPython/singleRelayIoc.py
@@ -0,0 +1,39 @@
+from IceCubePyClassIoc import GenericIOC
+import serial
+import json
+import time
+
+class SingleRelayIOC(GenericIOC):
+    def initialiseDevice(self):
+        self.serialCon = serial.Serial('/dev/cu.usbmodem1421', 9600)
+
+    def getDataFromDevice(self):
+        return None
+
+if __name__ == "__main__":
+    def handleIncomingMessage(client, serialCon, msg):
+        # handle messages from broker
+        if "/set/lamp" in msg.topic:
+            data = json.loads(str(msg.payload))
+
+            serialCommand = "P" + data['period'] + "\n"
+            print "Sending " + serialCommand + " to device"
+            serialCon.write(serialCommand)
+
+            time.sleep(0.1)
+
+            serialCommand = "T " + data['onTime'] + "\n"
+            print "Sending " + serialCommand + " to device"
+            serialCon.write(serialCommand)
+
+    homeSingleRelayIOC = SingleRelayIOC(brokerFile = 'itsmqttbroker.dat')
+
+    homeSingleRelayIOC.handleIncomingMessage = handleIncomingMessage
+    homeSingleRelayIOC.periodicPollPeriodSecs = 1
+    homeSingleRelayIOC.mqttStart(
+            clientId       = "homeSingleRelayIOC",
+            subscribeTopic = "homeSingleRelayIOC/set/#",
+            publishTopic   = "homeSingleRelayIOC/get")
+    homeSingleRelayIOC.client.user_data_set(homeSingleRelayIOC.serialCon)
+
+    homeSingleRelayIOC.run()
