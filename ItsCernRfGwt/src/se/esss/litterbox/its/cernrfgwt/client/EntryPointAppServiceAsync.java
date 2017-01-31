fbd3377d5a38c9e13554ecd36e24133f48d0cd2b
david-mcginnis <dmcginnis427@gmail.com>
2017-01-31 19:33:07 +0100

    Removed ItsCerRfGwt

--- ItsCernRfGwt/src/se/esss/litterbox/its/cernrfgwt/client/EntryPointAppServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package se.esss.litterbox.its.cernrfgwt.client;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface EntryPointAppServiceAsync 
-{
-	void checkIpAddress(boolean debug, AsyncCallback<String[]> callback);
-	void getModulatorProtocols(boolean debug, AsyncCallback<String[][]> callback);
-}
