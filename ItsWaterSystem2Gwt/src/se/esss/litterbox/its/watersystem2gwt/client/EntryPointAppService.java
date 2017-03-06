3f779b1ba0429f98953643ba01ee24182813961b
david-mcginnis <dmcginnis427@gmail.com>
2017-03-06 08:09:46 +0100

    First Commit of ITS Water System2

---
+++ ItsWaterSystem2Gwt/src/se/esss/litterbox/its/watersystem2gwt/client/EntryPointAppService.java
@@ -0,0 +1,14 @@
+package se.esss.litterbox.its.watersystem2gwt.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client-side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("entrypointapp")
+public interface EntryPointAppService extends RemoteService 
+{
+	String[] gskelServerTest(String name) throws Exception;
+	String[] checkIpAddress() throws Exception;
+}
