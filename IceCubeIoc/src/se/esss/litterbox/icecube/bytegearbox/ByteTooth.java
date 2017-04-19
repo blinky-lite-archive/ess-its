e7b2db5a2019840ce1cc4c9033fb1eb71225fd51
david-mcginnis <dmcginnis427@gmail.com>
2017-04-19 07:56:28 +0200

    Removed ByteBuffer from ByteTooth to get rid of nio library depend

--- IceCubeIoc/src/se/esss/litterbox/icecube/bytegearbox/ByteTooth.java
+++ IceCubeIoc/src/se/esss/litterbox/icecube/bytegearbox/ByteTooth.java
@@ -1,13 +1,10 @@
 package se.esss.litterbox.icecube.bytegearbox;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
 import org.json.simple.JSONObject;
 
 public class ByteTooth 
 {
 	
-	private static ByteOrder byteOrder =  ByteOrder.BIG_ENDIAN;
+	private static boolean littleEndian =  true;
 
 	private String name = "";
 	private String description = "";
@@ -59,9 +56,7 @@ public class ByteTooth
 		{
 			byte[] byteChunk = new byte[4];
 			for (int ii = 0; ii < 4; ++ii) byteChunk[ii] = byteArray[offset + ii];
-			ByteBuffer bb = ByteBuffer.wrap(byteArray);
-			bb.order(byteOrder);
-			value = Float.toString(bb.getFloat());
+			value = bytesToFloat(byteChunk, littleEndian);
 		}
 		if (toothType.equals("BYTE"))
 		{
@@ -71,33 +66,25 @@ public class ByteTooth
 		{
 			byte[] byteChunk = new byte[8];
 			for (int ii = 0; ii < 8; ++ii) byteChunk[ii] = byteArray[offset + ii];
-			ByteBuffer bb = ByteBuffer.wrap(byteArray);
-			bb.order(byteOrder);
-			value = Long.toString(bb.getLong());
+			value = bytesToLong(byteChunk, littleEndian);
 		}
 		if (toothType.equals("SHORT"))
 		{
 			byte[] byteChunk = new byte[2];
 			for (int ii = 0; ii < 2; ++ii) byteChunk[ii] = byteArray[offset + ii];
-			ByteBuffer bb = ByteBuffer.wrap(byteArray);
-			bb.order(byteOrder);
-			value = Short.toString(bb.getShort());
+			value = bytesToShort(byteChunk, littleEndian);
 		}
 		if (toothType.equals("INT"))
 		{
 			byte[] byteChunk = new byte[2];
 			for (int ii = 0; ii < 4; ++ii) byteChunk[ii] = byteArray[offset + ii];
-			ByteBuffer bb = ByteBuffer.wrap(byteArray);
-			bb.order(byteOrder);
-			value = Integer.toString(bb.getInt());
+			value = bytesToInt(byteChunk, littleEndian);
 		}
 		if (toothType.equals("DOUBLE"))
 		{
 			byte[] byteChunk = new byte[8];
 			for (int ii = 0; ii < 8; ++ii) byteChunk[ii] = byteArray[offset + ii];
-			ByteBuffer bb = ByteBuffer.wrap(byteArray);
-			bb.order(byteOrder);
-			value = Double.toString(bb.getDouble());
+			value = bytesToDouble(byteChunk, littleEndian);
 		}
 	}
 	public void loadDataIntoByteArray(byte[] byteArray, int byteGearWriteOffset) 
@@ -116,9 +103,7 @@ public class ByteTooth
 		}
 		if (toothType.equals("FLOAT"))
 		{
-			ByteBuffer bb = ByteBuffer.allocate(4);
-			bb.order(byteOrder);
-			byte[] byteChunk = bb.putFloat(Float.parseFloat(value)).array();
+			byte[] byteChunk = floatToBytes(value, littleEndian);
 			for (int ii = 0; ii < 4; ++ii) byteArray[offset + ii] = byteChunk[ii];
 		}
 		if (toothType.equals("BYTE"))
@@ -127,30 +112,22 @@ public class ByteTooth
 		}
 		if (toothType.equals("LONG"))
 		{
-			ByteBuffer bb = ByteBuffer.allocate(8);
-			bb.order(byteOrder);
-			byte[] byteChunk = bb.putLong(Long.parseLong(value)).array();
+			byte[] byteChunk = longToBytes(value, littleEndian);
 			for (int ii = 0; ii < 8; ++ii) byteArray[offset + ii] = byteChunk[ii];
 		}
 		if (toothType.equals("SHORT"))
 		{
-			ByteBuffer bb = ByteBuffer.allocate(2);
-			bb.order(byteOrder);
-			byte[] byteChunk = bb.putShort(Short.parseShort(value)).array();
+			byte[] byteChunk = shortToBytes(value, littleEndian);;
 			for (int ii = 0; ii < 2; ++ii) byteArray[offset + ii] = byteChunk[ii];
 		}
 		if (toothType.equals("INT"))
 		{
-			ByteBuffer bb = ByteBuffer.allocate(4);
-			bb.order(byteOrder);
-			byte[] byteChunk = bb.putInt(Integer.parseInt(value)).array();
+			byte[] byteChunk = intToBytes(value, littleEndian);;
 			for (int ii = 0; ii < 4; ++ii) byteArray[offset + ii] = byteChunk[ii];
 		}
 		if (toothType.equals("DOUBLE"))
 		{
-			ByteBuffer bb = ByteBuffer.allocate(8);
-			bb.order(byteOrder);
-			byte[] byteChunk = bb.putDouble(Double.parseDouble(value)).array();
+			byte[] byteChunk = doubleToBytes(value, littleEndian);;
 			for (int ii = 0; ii < 8; ++ii) byteArray[offset + ii] = byteChunk[ii];
 		}
 	}
@@ -166,6 +143,225 @@ public class ByteTooth
 		outputData.put("value", value);
 		outputData.put("writeable", writeable);
 		return outputData;
+	}
+	private static byte[] floatToBytes(String val, boolean littleEndian)
+	{
+		int bits = Float.floatToIntBits(Float.parseFloat(val));
+		byte[] bytes = new byte[4];
+		if (littleEndian)
+		{
+			bytes[0] = (byte)(bits & 0xff);
+			bytes[1] = (byte)((bits >> 8) & 0xff);
+			bytes[2] = (byte)((bits >> 16) & 0xff);
+			bytes[3] = (byte)((bits >> 24) & 0xff);
+		}
+		else
+		{
+			bytes[3] = (byte)(bits & 0xff);
+			bytes[2] = (byte)((bits >> 8) & 0xff);
+			bytes[1] = (byte)((bits >> 16) & 0xff);
+			bytes[0] = (byte)((bits >> 24) & 0xff);
+		}
+		return bytes;
+	}
+	private static String bytesToFloat(byte[] bytes, boolean littleEndian)
+	{
+		int asInt;
+		if (littleEndian)
+		{
+			asInt = (bytes[0] & 0xFF) 
+		            | ((bytes[1] & 0xFF) << 8) 
+		            | ((bytes[2] & 0xFF) << 16) 
+		            | ((bytes[3] & 0xFF) << 24);
+		}
+		else
+		{
+			asInt = (bytes[3] & 0xFF) 
+		            | ((bytes[2] & 0xFF) << 8) 
+		            | ((bytes[1] & 0xFF) << 16) 
+		            | ((bytes[0] & 0xFF) << 24);
+		}
+		return Float.toString(Float.intBitsToFloat(asInt));
+	}
+	private static byte[] intToBytes(String val, boolean littleEndian)
+	{
+		int bits = Integer.parseInt(val);
+		byte[] bytes = new byte[4];
+		if (littleEndian)
+		{
+			bytes[0] = (byte)(bits & 0xff);
+			bytes[1] = (byte)((bits >> 8) & 0xff);
+			bytes[2] = (byte)((bits >> 16) & 0xff);
+			bytes[3] = (byte)((bits >> 24) & 0xff);
+		}
+		else
+		{
+			bytes[3] = (byte)(bits & 0xff);
+			bytes[2] = (byte)((bits >> 8) & 0xff);
+			bytes[1] = (byte)((bits >> 16) & 0xff);
+			bytes[0] = (byte)((bits >> 24) & 0xff);
+		}
+		return bytes;
+	}
+	private static String bytesToInt(byte[] bytes, boolean littleEndian)
+	{
+		int asInt;
+		if (littleEndian)
+		{
+			asInt = (bytes[0] & 0xFF) 
+		            | ((bytes[1] & 0xFF) << 8) 
+		            | ((bytes[2] & 0xFF) << 16) 
+		            | ((bytes[3] & 0xFF) << 24);
+		}
+		else
+		{
+			asInt = (bytes[3] & 0xFF) 
+		            | ((bytes[2] & 0xFF) << 8) 
+		            | ((bytes[1] & 0xFF) << 16) 
+		            | ((bytes[0] & 0xFF) << 24);
+		}
+		return Integer.toString(asInt);
+	}
+	private static byte[] shortToBytes(String val, boolean littleEndian)
+	{
+		short bits = Short.parseShort(val);
+		byte[] bytes = new byte[2];
+		if (littleEndian)
+		{
+			bytes[0] = (byte)(bits & 0xff);
+			bytes[1] = (byte)((bits >> 8) & 0xff);
+		}
+		else
+		{
+			bytes[1] = (byte)(bits & 0xff);
+			bytes[0] = (byte)((bits >> 8) & 0xff);
+		}
+		return bytes;
+	}
+	private static String bytesToShort(byte[] bytes, boolean littleEndian)
+	{
+		int asInt = 0;
+		if (littleEndian)
+		{
+			asInt = (bytes[0] & 0xFF) 
+		            | ((bytes[1] & 0xFF) << 8);
+		}
+		else
+		{
+			asInt = (bytes[1] & 0xFF) 
+		            | ((bytes[0] & 0xFF) << 8); 
+		}
+		return Short.toString((short) asInt);
+	}
+	private static byte[] longToBytes(String val, boolean littleEndian)
+	{
+		long bits = Long.parseLong(val);
+		byte[] bytes = new byte[8];
+		if (littleEndian)
+		{
+			bytes[0] = (byte)(bits & 0xff);
+			bytes[1] = (byte)((bits >> 8) & 0xff);
+			bytes[2] = (byte)((bits >> 16) & 0xff);
+			bytes[3] = (byte)((bits >> 24) & 0xff);
+			bytes[4] = (byte)((bits >> 32) & 0xff);
+			bytes[5] = (byte)((bits >> 40) & 0xff);
+			bytes[6] = (byte)((bits >> 48) & 0xff);
+			bytes[7] = (byte)((bits >> 56) & 0xff);
+		}
+		else
+		{
+			bytes[7] = (byte)(bits & 0xff);
+			bytes[6] = (byte)((bits >> 8) & 0xff);
+			bytes[5] = (byte)((bits >> 16) & 0xff);
+			bytes[4] = (byte)((bits >> 24) & 0xff);
+			bytes[3] = (byte)((bits >> 32) & 0xff);
+			bytes[2] = (byte)((bits >> 40) & 0xff);
+			bytes[1] = (byte)((bits >> 48) & 0xff);
+			bytes[0] = (byte)((bits >> 56) & 0xff);
+		}
+		return bytes;
+	}
+	private static String bytesToLong(byte[] bytes, boolean littleEndian)
+	{
+		long asLong = 0;
+		if (littleEndian)
+		{
+			asLong = (bytes[0] & 0xFFL) 
+		            | ((bytes[1] & 0xFFL) << 8) 
+		            | ((bytes[2] & 0xFFL) << 16) 
+		            | ((bytes[3] & 0xFFL) << 24)
+		            | ((bytes[4] & 0xFFL) << 32)
+		            | ((bytes[5] & 0xFFL) << 40)
+		            | ((bytes[6] & 0xFFL) << 48)
+		            | ((bytes[7] & 0xFFL) << 56);
+		}
+		else
+		{
+			asLong = (bytes[7] & 0xFFL) 
+		            | ((bytes[6] & 0xFFL) << 8) 
+		            | ((bytes[5] & 0xFFL) << 16) 
+		            | ((bytes[4] & 0xFFL) << 24)
+		            | ((bytes[3] & 0xFFL) << 32)
+		            | ((bytes[2] & 0xFFL) << 40)
+		            | ((bytes[1] & 0xFFL) << 48)
+		            | ((bytes[0] & 0xFFL) << 56);
+		}
+		return Long.toString(asLong);
+	}
+	private static byte[] doubleToBytes(String val, boolean littleEndian)
+	{
+		long bits = Double.doubleToLongBits(Double.parseDouble(val));
+		byte[] bytes = new byte[8];
+		if (littleEndian)
+		{
+			bytes[0] = (byte)(bits & 0xff);
+			bytes[1] = (byte)((bits >> 8) & 0xff);
+			bytes[2] = (byte)((bits >> 16) & 0xff);
+			bytes[3] = (byte)((bits >> 24) & 0xff);
+			bytes[4] = (byte)((bits >> 32) & 0xff);
+			bytes[5] = (byte)((bits >> 40) & 0xff);
+			bytes[6] = (byte)((bits >> 48) & 0xff);
+			bytes[7] = (byte)((bits >> 56) & 0xff);
+		}
+		else
+		{
+			bytes[7] = (byte)(bits & 0xff);
+			bytes[6] = (byte)((bits >> 8) & 0xff);
+			bytes[5] = (byte)((bits >> 16) & 0xff);
+			bytes[4] = (byte)((bits >> 24) & 0xff);
+			bytes[3] = (byte)((bits >> 32) & 0xff);
+			bytes[2] = (byte)((bits >> 40) & 0xff);
+			bytes[1] = (byte)((bits >> 48) & 0xff);
+			bytes[0] = (byte)((bits >> 56) & 0xff);
+		}
+		return bytes;
+	}
+	private static String bytesToDouble(byte[] bytes, boolean littleEndian)
+	{
+		long asLong = 0;
+		if (littleEndian)
+		{
+			asLong = (bytes[0] & 0xFFL) 
+		            | ((bytes[1] & 0xFFL) << 8) 
+		            | ((bytes[2] & 0xFFL) << 16) 
+		            | ((bytes[3] & 0xFFL) << 24)
+		            | ((bytes[4] & 0xFFL) << 32)
+		            | ((bytes[5] & 0xFFL) << 40)
+		            | ((bytes[6] & 0xFFL) << 48)
+		            | ((bytes[7] & 0xFFL) << 56);
+		}
+		else
+		{
+			asLong = (bytes[7] & 0xFFL) 
+		            | ((bytes[6] & 0xFFL) << 8) 
+		            | ((bytes[5] & 0xFFL) << 16) 
+		            | ((bytes[4] & 0xFFL) << 24)
+		            | ((bytes[3] & 0xFFL) << 32)
+		            | ((bytes[2] & 0xFFL) << 40)
+		            | ((bytes[1] & 0xFFL) << 48)
+		            | ((bytes[0] & 0xFFL) << 56);
+		}
+		return Double.toString(Double.longBitsToDouble(asLong));
 	}
 
 }
